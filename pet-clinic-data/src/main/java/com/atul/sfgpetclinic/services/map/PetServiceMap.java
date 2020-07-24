235de7c3102102cbeb9e027333f23c3bc4730129
AtulGarg28 <500068649@stu.upes.ac.in>
2020-07-24 13:18:35 +0530

    Adding bootstrap for data. Closes #15

--- pet-clinic-data/src/main/java/com/atul/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/atul/sfgpetclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package com.atul.sfgpetclinic.services.map;
 
 import com.atul.sfgpetclinic.model.Pet;
-import com.atul.sfgpetclinic.services.CrudService;
+import com.atul.sfgpetclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet,Long> implements CrudService<Pet,Long> {
+public class PetServiceMap extends AbstractMapService<Pet,Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
--- pet-clinic-data/src/main/java/com/atul/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/atul/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,18 +1,17 @@
 package com.atul.sfgpetclinic.services.map;
 
 import com.atul.sfgpetclinic.model.Vet;
-import com.atul.sfgpetclinic.services.CrudService;
+import com.atul.sfgpetclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet,Long> implements CrudService<Vet,Long> {
+public class VetServiceMap extends AbstractMapService<Vet,Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
     }
 
     @Override
-
     public Vet findById(Long id) {
         return super.findById(id);
     }
