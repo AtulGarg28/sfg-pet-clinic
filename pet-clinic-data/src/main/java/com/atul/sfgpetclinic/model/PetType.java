e9529589abb262ab188e0053f4d0a2fca6176e93
AtulGarg28 <500068649@stu.upes.ac.in>
2020-08-12 12:02:33 +0530

    Added visits, updated owner with pet relationship.. closes #27.

--- pet-clinic-data/src/main/java/com/atul/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/atul/sfgpetclinic/model/PetType.java
@@ -1,4 +1,15 @@
 package com.atul.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class PetType extends BaseEntity{
+    private Set<Pet> petSet;
+
+    public Set<Pet> getPetSet() {
+        return petSet;
+    }
+
+    public void setPetSet(Set<Pet> petSet) {
+        this.petSet = petSet;
+    }
 }
