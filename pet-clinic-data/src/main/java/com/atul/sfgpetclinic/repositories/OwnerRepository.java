a0239a9eadc871984e405206831c3aee67906342
AtulGarg28 <500068649@stu.upes.ac.in>
2020-09-06 19:45:38 +0530

    Added spring data JPA service for owners. Closes #41.

--- pet-clinic-data/src/main/java/com/atul/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/atul/sfgpetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,5 @@ import com.atul.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner,Long> {
+    Owner findByLastName(String lastName);
 }
