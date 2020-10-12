2bd6fd8f5de50ca28ac99f112cfe045ed82e6957
AtulGarg28 <500068649@stu.upes.ac.in>
2020-10-12 11:13:35 +0530

    Dhowing pet type in display owners. Closes #64

--- pet-clinic-data/src/main/java/com/atul/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/atul/sfgpetclinic/model/PetType.java
@@ -20,6 +20,8 @@ public class PetType extends BaseEntity{
 
     @Override
     public String toString() {
-        return name;
+        return "PetType{" +
+                "name='" + name + '\'' +
+                '}';
     }
 }
