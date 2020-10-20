3175218057b2701874561de7cb7a6b3f5c930d0c
AtulGarg28 <500068649@stu.upes.ac.in>
2020-10-20 13:38:07 +0530

    Error in postMapping of Adding or updating the pet. #57

--- pet-clinic-data/src/main/java/com/atul/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/atul/sfgpetclinic/model/Pet.java
@@ -1,6 +1,7 @@
 package com.atul.sfgpetclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -16,9 +17,13 @@ import java.util.Set;
 public class Pet extends BaseEntity{
 
     @Builder
-    public Pet(Long id, String name){
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
         super(id);
-        this.name=name;
+        this.name = name;
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+        this.visits = visits;
     }
 
     @Column(name = "name")
@@ -33,6 +38,7 @@ public class Pet extends BaseEntity{
     private Owner owner;
 
     @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
--- pet-clinic-web/src/main/java/com/atul/sfgpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/com/atul/sfgpetclinic/controllers/PetController.java
@@ -8,9 +8,13 @@ import com.atul.sfgpetclinic.services.PetService;
 import com.atul.sfgpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Collection;
 
 @Controller
@@ -44,10 +48,50 @@ public class PetController {
     }
 
     @GetMapping("/pets/new")
-    public String addNewPet(Owner owner, Model model){
+    public String initAddNewPet(Owner owner, Model model){
         Pet pet=new Pet();
         owner.getPets().add(pet);
+        System.out.println(owner.getFirstName());
+
+        // To set owner for that pet.
+        pet.setOwner(owner);
         model.addAttribute("pet",pet);
         return CREATE_OR_UPDATE_PET;
+    }
+
+    @PostMapping("/pets/new")
+    public String processAddNewPet(Owner owner,Pet pet, BindingResult result, ModelMap modelMap){
+        if (StringUtils.hasLength(pet.getName()) && pet.isNew() && owner.getPet(pet.getName(),true)!=null ){
+            result.rejectValue("name", "duplicate", "already exists");
+        }
+        owner.getPets().add(pet);
+        if (result.hasErrors()){
+            modelMap.put("pet",pet);
+            return CREATE_OR_UPDATE_PET;
+        } else {
+            petService.save(pet);
+            return "redirect:/owners/"+owner.getId();
+        }
+    }
+
+    @GetMapping("/pets/{pet_id}/edit")
+    public String initUpdatePet(@PathVariable Long pet_id, Model model){
+        model.addAttribute("pet",petService.findById(pet_id));
+        return CREATE_OR_UPDATE_PET;
+    }
+
+    @PostMapping("/pets/{pet_id}/edit")
+    public String processUpdatePet(@Valid Pet pet,Owner owner,BindingResult result,Model model){
+        if (result.hasErrors()){
+            pet.setOwner(owner);
+            model.addAttribute("pet",pet);
+            return CREATE_OR_UPDATE_PET;
+        }
+        else {
+            owner.getPets().add(pet);
+            System.out.println(pet.getName());
+            petService.save(pet);
+            return "redirect:/owners/"+owner.getId();
+        }
     }
 }
