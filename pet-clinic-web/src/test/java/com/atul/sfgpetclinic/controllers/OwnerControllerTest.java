1d335d4dfebc917e24b13872a9ae4e5c51e2f143
AtulGarg28 <500068649@stu.upes.ac.in>
2020-10-27 13:11:36 +0530

    Adding test for Empty Owner Search. Closes #83

--- pet-clinic-web/src/test/java/com/atul/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/atul/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -15,8 +15,8 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasProperty;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -71,6 +71,17 @@ class OwnerControllerTest {
         mockMvc.perform(get("/owners"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("owners/ownersList"));
+    }
+
+    @Test
+    void processFindFormEmptyReturnManyOwnersTest() throws Exception {
+        when(ownerService.findAllByLastNameLike(anyString())).thenReturn(Arrays.asList(Owner.builder().id(1L).build(),Owner.builder().id(2L).build()));
+
+        mockMvc.perform(get("/owners")
+        .param("lastName",""))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownersList"))
+                .andExpect(model().attribute("selections",hasSize(2)));
     }
 
     @Test
